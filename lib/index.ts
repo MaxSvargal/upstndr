3d315c32b9bc66b175d4cab5bc147fdece51df1b
MaxSvargal <maxsvargal@gmail.com>
2018-08-08 11:46:19 +0300

    Add new exports of internals modules

--- lib/index.ts
+++ lib/index.ts
@@ -1,15 +1,22 @@
-import { createAction, createReducer } from 'redux-act'
+import { combineReducers } from 'redux'
 import { compose } from 'redux'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router'
+import { createAction, createReducer } from 'redux-act'
+import { createSelector, createStructuredSelector } from 'reselect'
 
 import injectSaga from '../app/utils/injectSaga'
 import injectReducer from '../app/utils/injectReducer'
 
 export {
-  createAction,
-  createReducer,
-  injectSaga,
-  injectReducer,
+  combineReducers,
   compose,
   connect,
+  withRouter,
+  createAction,
+  createReducer,
+  createSelector,
+  createStructuredSelector,
+  injectReducer,
+  injectSaga,
 }
