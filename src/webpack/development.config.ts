386e1fe7eef31a83298f10f0f139b9b8a5759452
MaxSvargal <maxsvargal@gmail.com>
2018-09-15 22:17:26 +0300

    Add url-loader for images to webpack configs

--- src/webpack/development.config.ts
+++ src/webpack/development.config.ts
@@ -31,6 +31,18 @@ export default <Configuration>{
           },
         ],
       },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // Inline files smaller than 10 kB
+              limit: 10 * 1024
+            }
+          }
+        ]
+      }
     ]
   },
   resolve: {
--- src/webpack/production.config.ts
+++ src/webpack/production.config.ts
@@ -32,6 +32,18 @@ export default <Configuration>{
           },
         ],
       },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // Inline files smaller than 10 kB
+              limit: 10 * 1024
+            }
+          }
+        ]
+      }
     ]
   },
   resolve: {
